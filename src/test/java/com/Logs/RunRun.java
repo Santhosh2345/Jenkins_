35d2f54c1fdeca4c1b097e3b9dc54d147b382de8
Santhosh2345 <sk3505712@gmail.com>
2024-06-15 14:21:59 +0530

    test

--- src/test/java/com/Logs/RunRun.java
+++ src/test/java/com/Logs/RunRun.java
@@ -1,20 +1,21 @@
 package com.Logs;
 
 import org.apache.logging.log4j.LogManager;
-
 import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 public class RunRun {
 	
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		RunRun a = new RunRun();                                                     
 		a.main2();
 	}
 
 	@Test
- public void main2() {
+ public void main2() throws InterruptedException {
 		
 		Logger log = LogManager.getLogger(this.getClass());
 		
@@ -26,6 +27,10 @@ public class RunRun {
 		log.trace("trace-1");
 		log.warn("Warning-1");
 		
-		
+		WebDriver driver = new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.amazon.in");
+		Thread.sleep(3000);
+		driver.quit();
 	}
 }
